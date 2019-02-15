7d1396c7b857dac079b6706d67df221d956e1317
liaojb <1161086097@qq.com>
2019-02-15 12:02:04 +0800

    test

--- sbdemo/src/main/java/com/ljb/sbdemo/controller/HelloController.java
+++ sbdemo/src/main/java/com/ljb/sbdemo/controller/HelloController.java
@@ -2,18 +2,19 @@ package com.ljb.sbdemo.controller;
 
 import com.ljb.sbdemo.models.model.UserDTO;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/hello")
 public class HelloController {
 
-    @RequestMapping("/sayHello")
+    @RequestMapping(value = "/sayHello", method = RequestMethod.POST)
     public String sayHello(){
         return "hello springboot";
     }
 
-    @RequestMapping("/getUser")
+    @RequestMapping(value = "/getUser", method = RequestMethod.POST)
     public UserDTO getUser(){
         UserDTO u = new UserDTO();
         u.setId("testId");
