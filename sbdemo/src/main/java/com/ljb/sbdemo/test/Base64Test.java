3af2887a7ae7d10f23fed49b481e7e226ca3f7c3
HutuBing <1161086097@qq.com>
2020-04-02 15:31:19 +0800

    f

--- sbdemo/src/main/java/com/ljb/sbdemo/test/Base64Test.java
+++ sbdemo/src/main/java/com/ljb/sbdemo/test/Base64Test.java
@@ -1,26 +1,11 @@
 package com.ljb.sbdemo.test;
 
-import sun.misc.BASE64Encoder;
 
 import java.io.File;
 import java.io.FileInputStream;
 
 public class Base64Test {
     public static String base64(int i){
-        try {
-            File file;
-            if (i == 0)
-                file = new File("C:\\Users\\Administrator\\Desktop\\test5.mp4");
-            else
-                file = new File("C:\\Users\\Administrator\\Desktop\\test.jpg");
-            FileInputStream inputFile = new FileInputStream(file);
-            byte[] buffer = new byte[(int) file.length()];
-            inputFile.read(buffer);
-            inputFile.close();
-            return (new BASE64Encoder().encode(buffer));
-        }catch (Exception e){
-            e.printStackTrace();
-            return "";
-        }
+        return "";
     }
 }
